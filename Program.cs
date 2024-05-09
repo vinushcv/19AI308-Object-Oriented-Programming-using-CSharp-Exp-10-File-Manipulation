f4e31621ae42283f28b6ebd62e2d25a266f07130
Aswini-J <aswinij@saveetha.ac.in>
2024-05-09 11:46:44 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+    struct student
+    {
+           // Write Your Code Here
+    };
+    class Tutorial
+    {
+
+        static void Main(string[] args)
+        {
+            // Write Your Code Here
+        }
+    }
+}
